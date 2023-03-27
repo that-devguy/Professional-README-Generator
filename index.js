d45474885beaaf82a8bef986072e2cc66709d601
that devguy <zkmutch@gmail.com>
2023-03-27 19:11:32 -0400

    added functions to generate license badge and the license section based on the user inputs. Added checkbox and list prompt for some questions.

--- index.js
+++ index.js
@@ -9,16 +9,45 @@ const questions = [
             type: 'input',
             message: 'What is the title of your project?',
             name: 'title',
+            validate: (value) => {if(value){return true} else {return 'Please give your project a title.'}}
+        },
+        {
+            type: 'list',
+            message: 'Please choose your license.',
+            name: 'license',
+            choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'BSD 3-clause', 'Unlicense', 'Other'],
+            default: 'MIT',
         },
         {
             type: 'input',
-            message: 'Please write a description of your project',
+            message: 'Please write a description of your project.',
             name: 'desc',
+            validate: (value) => {if(value){return true} else {return 'Please give your project a brief description, you can edit this later.'}}
         },
         {
-            type: 'input',
-            message: 'What technologies does your project use?',
+            type: 'checkbox',
+            message: 'What technologies does your project use?(You can add anything we missed after your README has been generated)',
             name: 'technologies',
+            choices: [
+              {name: 'HTML5'},
+              {name: 'CSS'},
+              {name: 'JavaScript'},
+              {name: 'Node.js'},
+              {name: 'Angular'},
+              {name: 'Vue.js'},
+              {name: 'Express'},
+              {name: 'MongoDB'},
+              {name: 'MySQL'},
+              {name: 'PostgreSQL'},
+              {name: 'Git'},
+              {name: 'GitHub Actions'},
+              {name: 'Travis CI'},
+              {name: 'Heroku'},
+              {name: 'AWS'},
+              {name: 'Docker'},
+              {name: 'Kubernetes'},
+            ],
+            validate: (value) => {if(value){return true} else {return 'Please select atleast one of the technologies used to build your project.'}}
         },
         {
             type: 'input',
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,57 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'GPLv2') {
+    return '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+  } else if (license === 'Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  } else if (license === 'GPLv3') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  } else if (license === 'BSD 3-clause') {
+    return '[![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+  } else if (license === 'Unlicense') {
+    return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+  } else if (license === 'Other') {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `## License
+  ${renderLicenseBadge(license)}
+  This project is licensed under the ${license} license. Feel free to edit and distribute this template as you like.
+  See LICENSE for more information.`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  
   ${data.desc}
   
+  
+  ## Table of Contents
+  - [Technologies Used](#technologies-used)
+  - [Installation](#installation-instructions)
+  - [Application Demo](#demo)
+  - [Links](#links)
+  - [License](#license)
+
   ## Technologies Used
 
-  ${data.technologies}
+  ${data.technologies.join(', ')}
+
+  ## Installation Instructions
+  
   
   ## ${data.title} Demo
   
@@ -26,8 +59,10 @@ function generateMarkdown(data) {
   
   ## Links
   
-  Deployed Application: ${data.appURL}
+  Deployed Application: ${data.appURL} 
   Github Repository: ${data.repoURL}
+
+  ${renderLicenseSection(data.license)}
 `;
 }
 
